81a557fdb110616780abf3ae659f1d8424c9a100
Harshh-byte <rockstarharsh7jan@gmail.com>
2025-06-04 13:03:11 +0530

    Issues handling

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const searchToggle = document.getElementById('search-icon-toggle');
 const searchContainer = document.getElementById('mobile-search-container');
 
-searchToggle.addEventListener('click', () => {
-  searchContainer.classList.toggle('hidden');
-});
+if (searchToggle && searchContainer) {
+  searchToggle.addEventListener('click', () => {
+    searchContainer.classList.toggle('hidden');
+  });
+}
 
 const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 
-menuToggle.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
-});
+if (menuToggle && mobileMenu) {
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+}
